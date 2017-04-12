9a83e23ef8cef5b82be32469a1b4e47203c92e16
Sabine Lorius <sl80caxe@studserv.uni-leipzig.de>
2017-04-12 12:45:48 +0200

    rdogh

--- src/Aufgabe02/Wuerfelexperiment.java
+++ src/Aufgabe02/Wuerfelexperiment.java
@@ -5,7 +5,7 @@ import java.math.MathContext;
 
 /**
  * Klasse, die ein Wuerfelexperiment simuliert.
- * kjlkjlkjlkjlkjkljk
+ * kjlkjlkjlkjlkjkljkfojj
  * @author Sabine Lorius und Thomas Pause
  */
 
