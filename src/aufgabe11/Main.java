e0fab28eaf540d6c2d2570885b2b570da5da79a9
@T-P-1 <tp55sude@studserv.uni-leipzig.de>
2017-04-13 15:40:46 +0200

    -template aufgabe 11

---
+++ src/aufgabe11/Main.java
@@ -0,0 +1,10 @@
+package aufgabe11;
+
+/**
+ * Die Main Methode. Einsprungpunkt des Programms.
+ * Hier entsteht Aufgabe 1.1 Numerische Integration
+ *
+ * @author Sabine Lorius und Thomas Pause
+ */
+public class Main {
+}
